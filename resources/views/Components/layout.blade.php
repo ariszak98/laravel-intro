c8df1b3f534cd52de5c504314ff6ff04cf3318c1
ariszak98 <ariszak98@icloud.com>
2025-11-18 18:56:03 +0200

    Initial commit

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+      
+       
+    </head>
+    <body class=" dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+      
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </nav>
+       
+        {{ $slot }}
+
+    </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+
+    <h1>Welcome to the Home page!</h1>
+
+</x-layout>
