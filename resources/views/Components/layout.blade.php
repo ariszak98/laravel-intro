e049ec61d3ce710de3bf9d0857a8bf2749fa2fa8
ariszak98 <ariszak98@icloud.com>
2025-11-18 19:18:20 +0200

    Fixed <nav>

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -16,9 +16,9 @@
     <body class=" dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
       
         <nav>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
+            <x-nav-link href="/" >Home</x-nav-link>
+            <x-nav-link href="/about" >About</x-nav-link>
+            <x-nav-link href="/contact" >Contact</x-nav-link>
         </nav>
        
         {{ $slot }} 
