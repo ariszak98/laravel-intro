02e70a058e0fd8c8564a52bb6c63d7ff67394d60
ariszak98 <ariszak98@icloud.com>
2025-11-19 20:02:14 +0200

    Added Job Model and created its migration

--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,24 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        
-        "jobs" => [
-            [
-                "id"        => 1,
-                "title"     => "Director",
-                "salary"    => "$100,000"
-            ],
-            [
-                "id"        => 2,
-                "title"     => "Programmer",
-                "salary"    => "$10,000"
-            ],
-            [
-                "id"        => 3,
-                "title"     => "Musician",
-                "salary"    => "$60,000"
-            ]
-        ]
-    ]);
+    return view('jobs', ["jobs" => Job::all()]);
 });
 
 Route::get('/contact', function (){
     return view('contact');
 });
 
-Route::get('/jobs/{id}', function ($id){
+Route::get('/jobs/{id}', function ($id) {
 
-    $jobs = [
-            [
-                "id"        => 1,
-                "title"     => "Director",
-                "salary"    => "$100,000"
-            ],
-            [
-                "id"        => 2,
-                "title"     => "Programmer",
-                "salary"    => "$10,000"
-            ],
-            [
-                "id"        => 3,
-                "title"     => "Musician",
-                "salary"    => "$60,000"
-            ]
-    ];
-
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);    
+    $job = Job::find($id);  
 
     return view('job', ['job' => $job]);
 });
