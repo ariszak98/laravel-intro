5abaae9c0a7149224d80f9a7bbabffa4db429224
ariszak98 <ariszak98@icloud.com>
2025-11-21 19:55:12 +0200

    hasMany & belongsTo Relationships

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Job;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    public function jobs() {
+        return $this->hasMany(Job::class);
+    }
 }
