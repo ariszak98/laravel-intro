056e50c4d7b36a6057c6e0aa7fab4e7a2d860c83
ariszak98 <ariszak98@icloud.com>
2025-11-25 22:57:17 +0200

    Job Resource & Routes cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,96 +1,12 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {    
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
 
-Route::get('/contact', function (){
-    return view('contact');
-});
-
-// Index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->paginate(4);
-    return view('jobs/index', ["jobs" => $jobs]);
-});
-
-// Create
-Route::get('/jobs/create', function () {
-    return view('/jobs/create');
-});
-
-
-// Show
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);  
-    return view('jobs/show', ['job' => $job]);
-});
-
-
-// Edit
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);  
-    return view('jobs/edit', ['job' => $job]);
-});
-
-
-// Update
-Route::patch('/jobs/{id}', function ($id) {
-    
-    // validate
-    request()->validate([
-        'title'     =>  ['required', 'min:3'],
-        'salary'    =>  ['required']
-    ]);
-
-    // authorize (On hold..)
-    // update the job
-    $job = Job::findOrFail($id);
-    $job->update([
-        'title'     => request('title'),
-        'salary'    => request('salary')
-    ]);
-
-    // redirect to the job page
-    return redirect('/jobs/' . $job->id);
-
-});
-
-
-// Destroy
-Route::delete('/jobs/{id}', function ($id) {
-
-    // authorize (on hold..)
-
-    $job = Job::findOrFail($id);
-    $job->delete();
-
-    // OR 
-    // Job::findOrFail($id)->delete();
-
-    return redirect('/jobs');
-    
-});
-
-
-// Store
-Route::post('/jobs', function (){
-
-    request()->validate([
-        'title'     =>  ['required', 'min:3'],
-        'salary'    =>  ['required']
-    ]);
-
-    Job::create([
-        'title'     =>  request('title'),
-        'salary'    =>  request('salary'),
-        'employer_id'   => 1
-    ]);
-
-    return redirect('/jobs');
-});
+// Job Resource
+Route::resource('jobs', JobController::class);
