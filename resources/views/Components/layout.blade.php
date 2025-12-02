2945d9745e7b8f3ef918577709c9badf69ddffff
ariszak98 <ariszak98@icloud.com>
2025-12-02 19:44:47 +0200

    Vite, Tailwind

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -10,8 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-      
-       <script src="https://cdn.tailwindcss.com" ></script>
+        @vite('resources/css/app.css')
+
     </head>
     <body class="h-full">
 
